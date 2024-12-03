d567dd68a661a2ea79200d11a8918f0fbff7c0c8
Nataliia Smirnova <ensueno11@gmail.com>
2024-12-03 23:31:41 +0200

    make build

---
+++ index.34199799.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),c=t.clientHeight,l=t.clientWidth,o=e.offsetHeight,i=e.offsetWidth;e.style.top="".concat((c-o)/2,"px"),e.style.left="".concat((l-i)/2,"px");
+//# sourceMappingURL=index.34199799.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const spider = document.querySelector(".spider");
+const wall = document.querySelector(".wall");
+const wallHeight = wall.clientHeight;
+const wallWidth = wall.clientWidth;
+const spiderHeight = spider.offsetHeight;
+const spiderWidth = spider.offsetWidth;
+spider.style.top = `${(wallHeight - spiderHeight) / 2}px`;
+spider.style.left = `${(wallWidth - spiderWidth) / 2}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
